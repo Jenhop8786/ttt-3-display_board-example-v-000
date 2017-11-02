bc6aaade2e3e4192b4505fa17fc14bffb5c445f4
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2017-11-02 18:01:31 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 def display_board
-  puts row_1 "    |  |  "
-  puts row_2 " ----------- "
-  puts row_3 "    |  |  "
-  puts row_4 " ----------- "
-  puts row_5 "    |  |  "
+  puts  "    |  |  "
+  puts  " ----------- "
+  puts  "    |  |  "
+  puts  " ----------- "
+  puts  "    |  |  "
 end
 display_board
